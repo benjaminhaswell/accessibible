0a82718835400bbd0201dd5c84c413b6b33ef982
Ben Haswell <benjaminallardhaswell@gmail.com>
2023-11-07 22:52:05 -0500

    Vite/config setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <img src="/logos/2x.png" alt="Accessibible Logo" />
+      <h1>Welcome to Accessibible</h1>
+    </>
+  )
+}
+
+export default App
