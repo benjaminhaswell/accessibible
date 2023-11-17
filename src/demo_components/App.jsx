33193b5a86e07970499d92b6397e01dc8cc3cba9
Ben Haswell <benjaminallardhaswell@gmail.com>
2023-11-17 18:18:37 -0500

    Basic homepage - add cards

--- src/demo_components/App.jsx
+++ src/demo_components/App.jsx
@@ -4,8 +4,16 @@ function App() {
 
   return (
     <>
-      <img src="/logos/2x.png" alt="Accessibible Logo" />
-      <h1>Welcome to Accessibible</h1>
+      <div className="text-center mx-auto">
+      <h1 className="mt-8">Welcome to Accessibible Demonstrations</h1>
+      <div className='mt-12'>
+        <p className='mt-4'>This set of pages will cover all kinds of common barriers people experience on the web.</p>
+        <p className='mt-4'>Each page will consist of simple explanations of the topic, demonstrations so you can step into the shoes
+          of somebody running into that particular barrier, as well as code samples to provide concrete suggestions for
+          preventing these issues in the first place.
+        </p>
+      </div>
+    </div>
     </>
   )
 }
